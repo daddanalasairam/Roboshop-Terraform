b4b642ac1acdcf5c5a8fc11b0110266f3b604428
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-07 15:38:36 +0530

    Terraform Commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami = "ami-0b4f379183e5706b9"
-  instance_type = "t3.small"
+  instance_type = var.instance_type
   vpc_security_group_ids = ["sg-0d386c7e59b2416fe"]
   tags = {
     Name = "${var.component_name}-${var.env}"
