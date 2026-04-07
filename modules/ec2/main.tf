700b09d1de04773a3c0b79bfb1486472619e2d88
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-07 15:52:52 +0530

    Terraform Commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,32 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.component_name}-${var.env}-sg"
+  description = "Inbound allow for ${var.component_name}"
+
+  # inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # allow SSH from anywhere
+  }
+
+  ingress {
+    from_port   = var.app_port #80
+    to_port     = var.app_port #80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # allow HTTP from anywhere
+  }
+
+  # outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # all protocols
+    cidr_blocks = ["0.0.0.0/0"]  # allow all outbound
+  }
+}
+
+
 resource "aws_instance" "instance" {
   ami = "ami-0b4f379183e5706b9"
   instance_type = var.instance_type
