931f42d407431222f236ba9fd06cdd0a6747717b
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-15 21:48:57 +0530

    Commit

--- practice.tf
+++ practice.tf
@@ -12,3 +12,31 @@ resource "aws_instance" "instance" {
     Name = var.display_name
   }
 }
+
+variable "names" {
+  default = ["frontend", "backend"]
+}
+
+resource "aws_instance" "instance" {
+  count = 2
+
+  ami = var.ami_id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.names[count.index]
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = {
+    frontend = "t2.micro"
+    backend = "t2.micro"
+
+    ami = var.ami_id
+
+    tags = {
+      Name = each.key
+    }
+  }
+}
